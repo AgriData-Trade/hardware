dc6d5e21e9d2517e6da804b69850f7998af36ea9
Leon Bowie <leon@bowie-co.nz>
2022-01-10 10:20:23 +1300

    fix timeout due to network disconnect

--- agridata/agridata.ino
+++ agridata/agridata.ino
@@ -11,53 +11,41 @@ const int port = PORT;
 const unsigned long interval = MINUTE_DELAY * 60 * 1000;
 unsigned long previousMillis = interval;
 
-// initialize the library instance
+// setup network access
 NBClient client;
 GPRS gprs;
 NB nbAccess;
 
+// mqtt client
 MqttClient mqttClient(client);
 
 void setup() {
   // initialize serial communications and wait for port to open:
   Serial.begin(9600);
-  // while (!Serial) {
-  //   ; // wait for serial port to connect. Needed for native USB port only
-  // }
 
   Serial.println("Starting AgriData Sensor Node.");
 
 
   Serial.println("Warming up....");
-  // connection state
-  boolean connected = false;
-
-  while (!connected) {
-    if ((nbAccess.begin() == NB_READY) &&
-      (gprs.attachGPRS() == GPRS_READY)) {
-      connected = true;
-    }
-    else {
-      Serial.println("Not connected");
-      delay(1000);
-    }
+  Serial.println("Connecting to network.");
+  //Connects to network to use getTime()
+  if (nbAccess.status() != NB_READY || gprs.status() != GPRS_READY) {
+    connectNB();
   }
+  Serial.println("Connected to network.");
 
-  Serial.println("Connected to Cellular network.");
   Serial.print("Sending messages at an interval of: ");
   Serial.print(interval);
   Serial.println(" milliseconds.");
 
   Serial.println("Connecting to mqtt broker.");
-    if (!mqttClient.connect(broker, port)) {
-      Serial.print("MQTT connection failed! Error code = ");
-      Serial.println(mqttClient.connectError());
-    }
-    mqttClient.setTimeout(0);
-    Serial.println("Connected to mqtt broker.");
+  if (!mqttClient.connected()) {
+    // MQTT client is disconnected, connect
+    connectMQTT();
+  }
 
-    // generate random number seed
-    randomSeed(analogRead(0));
+  // generate random number seed
+  randomSeed(analogRead(0));
 }
 
 void loop() {
@@ -66,9 +54,16 @@ void loop() {
   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
+    Serial.println("******Sending Data to MQTT - STARTED******");
     // save the last time a message was sent
     previousMillis = currentMillis;
-
+    if (nbAccess.status() != NB_READY || gprs.status() != GPRS_READY) {
+    connectNB();
+  }
+    if (!mqttClient.connected()) {
+    // MQTT client is disconnected, connect
+    connectMQTT();
+  }
 
     int temp = random(10, 30);
     Serial.print("Sending message to topic: (");
@@ -78,7 +73,37 @@ void loop() {
     mqttClient.beginMessage(topic + "temperature");
     mqttClient.print(temp);
     mqttClient.endMessage();
-    Serial.println("Sent Message.\n");
-
+    Serial.println("******Sending Data to MQTT - COMPLETED******");
   }
+}
+
+void connectNB() {
+  Serial.println("******Connecting to Cellular Network - STARTED******");
+
+  while ((nbAccess.begin() != NB_READY) ||
+         (gprs.attachGPRS() != GPRS_READY)) {
+    // failed, retry
+    Serial.print("NB");
+    delay(1000);
+  }
+
+  Serial.println("******Connecting to Cellular Network - COMPLETED******");
+  Serial.println();
+}
+
+void connectMQTT() {
+ Serial.println("******Connecting to MQTT Broker - STARTED******");
+  Serial.print(broker);
+
+  while (!mqttClient.connect(broker, port)) {
+    // failed, retry
+    Serial.print(".");
+    Serial.println(mqttClient.connectError());
+    delay(5000);
+    if (nbAccess.status() != NB_READY || gprs.status() != GPRS_READY) {
+    connectNB();
+  } 
+  }
+  Serial.println("\n******Connecting to MQTT Broker - COMPLETED******");
+  Serial.println();
 }
